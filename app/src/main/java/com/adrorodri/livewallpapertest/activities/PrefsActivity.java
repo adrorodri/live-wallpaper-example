837e57fa81a411d7ba4d8f0adae68a42945362c1
Adrian Rodriguez <adrorodri@gmail.com>
2019-01-25 19:17:26 -0400

    Prefs activity fix

--- app/src/main/java/com/adrorodri/livewallpapertest/activities/PrefsActivity.java
+++ app/src/main/java/com/adrorodri/livewallpapertest/activities/PrefsActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
+import com.adrorodri.livewallpapertest.R;
 import com.adrorodri.livewallpapertest.fragments.PrefsFragment;
 
 public class PrefsActivity extends AppCompatActivity {
@@ -14,8 +15,9 @@ public class PrefsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prefs);
         prefsFragment = new PrefsFragment();
         FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction().add(prefsFragment, "PREFS_FRAGMENT").commitAllowingStateLoss();
+        fragmentManager.beginTransaction().add(R.id.fragment_container, prefsFragment, "PREFS_FRAGMENT").commitAllowingStateLoss();
     }
 }
